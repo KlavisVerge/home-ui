611faa4bb47c7e2ca67a246884b4b6588acea79a
Nick Phillips <imprecatorr@gmail.com>
2018-07-30 22:31:06 -0500

    Gateway change

--- src/home-ui-app/home-ui-app.js
+++ src/home-ui-app/home-ui-app.js
@@ -79,61 +79,66 @@ class HomeUiApp extends PolymerElement {
 
   ready() {
     super.ready();
-    var url = 'https://3oemw4weak.execute-api.us-east-1.amazonaws.com/api/twitch-games';
-      var data = {games: this.games};
-      let err = false;
+    let requestParams = '';
+    for(var i = 0; i < this.games.length; i++){
+      requestParams += this.games[i] + '%7C';
+    }
+    if(this.games.length > 0){
+      requestParams = requestParams.substring(0, requestParams.length - 3);
+    }
+    var url = 'https://xupmhdl2g5.execute-api.us-east-1.amazonaws.com/api/twitch-games?games=' + requestParams;
+    let err = false;
 
-      fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers:{
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+    fetch(url, {
+      method: 'GET',
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .catch(error => {
+      this.active = false;
+      this.$.spinner.classList.remove('active');
+      console.error('Error:', error);
+      err = true;
+    })
+    .then(response => {
+      if(err){
+        return;
+      }
+      for(var i = 0; i < response.game.data.length; i++){
+        let url = response.game.data[i].box_art_url.replace('{width}', window.innerWidth).replace('{height}',  window.innerHeight);
+        if(i === 0){
+          this.current = url;
+          this.current = url;
+          this.height = window.innerHeight - 17 + 'px';
+          this.width = window.innerWidth - 17 + 'px';
         }
-      }).then(res => res.json())
-      .catch(error => {
-        this.active = false;
-        this.$.spinner.classList.remove('active');
-        console.error('Error:', error);
-        err = true;
-      })
-      .then(response => {
-        if(err){
-          return;
+        this.allimages.push(url);
+      }
+      let pol = this;
+      window.addEventListener("resize", function(event) {
+        let i = 0;
+        while(i < pol.allimages.length){
+          pol.allimages[i] = response.game.data[i].box_art_url.replace('{width}', window.innerWidth).replace('{height}',  window.innerHeight);
+          if(i === pol.currentlocation){
+            pol.current = pol.allimages[pol.currentlocation];
+            pol.width = window.innerWidth - 17 + 'px';
+            pol.height = window.innerHeight - 17 + 'px';
+          }
+          i++;
         }
-        for(var i = 0; i < response.game.data.length; i++){
-          let url = response.game.data[i].box_art_url.replace('{width}', window.innerWidth).replace('{height}',  window.innerHeight);
-          if(i === 0){
-            this.current = url;
-            this.current = url;
-            this.height = window.innerHeight - 17 + 'px';
-            this.width = window.innerWidth - 17 + 'px';
-          }
-          this.allimages.push(url);
-        }
-        let pol = this;
-        window.addEventListener("resize", function(event) {
-          let i = 0;
-          while(i < pol.allimages.length){
-            pol.allimages[i] = response.game.data[i].box_art_url.replace('{width}', window.innerWidth).replace('{height}',  window.innerHeight);
-            if(i === pol.currentlocation){
-              pol.current = pol.allimages[pol.currentlocation];
-              pol.width = window.innerWidth - 17 + 'px';
-              pol.height = window.innerHeight - 17 + 'px';
-            }
-            i++;
-          }
-        });
-        setInterval(function(poly){
-          poly.currentlocation++;
-          if(poly.currentlocation === pol.allimages.length){
-            poly.currentlocation = 0;
-          }
-          poly.current = poly.allimages[poly.currentlocation];
-          poly.height = window.innerHeight - 17 + 'px';
-          poly.width = window.innerWidth - 17 + 'px';
-        }, 5000, this);
       });
+      setInterval(function(poly){
+        poly.currentlocation++;
+        if(poly.currentlocation === pol.allimages.length){
+          poly.currentlocation = 0;
+        }
+        poly.current = poly.allimages[poly.currentlocation];
+        poly.height = window.innerHeight - 17 + 'px';
+        poly.width = window.innerWidth - 17 + 'px';
+      }, 5000, this);
+    });
   }
 }
 
